94990c186d4131342b9548ab4422f49304448ca4
YuyaYoshimura <yuyayoshimura2003@gmail.com>
2022-05-29 09:53:31 +0900

    初コミット

---
+++ Calculate_ip_checksum.h
@@ -0,0 +1,25 @@
+#include <cstdint>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
+
+class Calculate_ip_checksum {
+public:
+	uint16_t checksum;
+public:
+	Calculate_ip_checksum(struct iphdr *p);
+};
+
+Calculate_ip_checksum::Calculate_ip_checksum(struct iphdr *p)
+{
+	p->check = 0;
+	uint16_t *tmp = (uint16_t *)p;
+	int result = 0;
+
+	for (int i = 1; i <= 10; i++) {
+		result += *tmp;
+		tmp++;
+	}
+
+	result = result + (result >> 16);
+	checksum = htons(~result);
+}
